9fe54a0c028117f723eff823edcdfb559f0b5c0d
Darshita Patel <pateldarshita1994@gmail.com>
2024-12-12 02:20:58 -0500

    LightBnB Web App added

--- 1_queries/4_all_my_reservations.sql
+++ 1_queries/4_all_my_reservations.sql
@@ -6,15 +6,3 @@ WHERE reservations.guest_id = 1
 GROUP BY reservations.id, properties.id
 ORDER BY reservations.start_date
 LIMIT 10;
-
-
-
-
-
-
-
--- Select the reservation id, property title, reservation start_date, property cost_per_night and the average rating of the property. 
--- You'll need data from both the reservations and properties tables.
--- The reservations will be for a single user, so just use 1 for the user_id.
--- Order the results from the earliest start_date to the most recent start_date.
--- Limit the results to 10.
